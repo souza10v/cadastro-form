73c59be3933273c9d5f2fe29283710fd1d421147
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-10-26 15:12:09 -0300

    upadate to verify same email and same username

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,11 +3,13 @@ const { v4: uuidv4 } = require('uuid');
 
 const createUser = async (req, res) => {
     const { username, password, email, first_name, last_name } = req.body;
-    const user_id = uuidv4();
+    const userID = uuidv4();
+
+    console.log(userID)
 
     try {
-        const newUser = await userService.createUser(username, password, email, first_name, last_name, user_id);
-        res.json('Created successfully');
+        const {newUser, errorActual} = await userService.createUser(username, password, email, first_name, last_name, userID);
+        res.json({ message: 'Created successfully', error: errorActual })
     } catch (error) {
         console.log(`Error creating id: ${error}`)
     }
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,8 +1,9 @@
 const { v4: uuidv4 } = require('uuid');
 const pool = require('../db');
 
-
 const isUserIDUnique = async (userID) => {
+
+    const error = ""
     let isUnique = false;
 
     while (!isUnique) {
@@ -14,25 +15,35 @@ const isUserIDUnique = async (userID) => {
             } else {
                 userID = uuidv4();
             }
-            
+
         } catch (error) {
-            console.log(`Error getting id ${error}`)
+
+            if (error.code === '23505') {
+                if (error.detail.includes('email')) {
+                    console.log('Email already exists.');
+                    error = 'Email already exists.';
+                } else if (error.detail.includes('username')) {
+                    console.log('Username already exists.');
+                    error = 'Username already exists.';
+                } else {
+                    console.log(`Other unique constraint violation: ${error.detail}`);
+                    error = `Other unique constraint violation: ${error.detail}`;
+                }
+                console.log(`Error getting id ${error}`)
+            }
         }
-
-        
     }
-
-    return userID;
+    console.log(userID)
+    return { userID, error };
 };
 
 const createUser = async (username, password, email, firstName, lastName, userID) => {
     //  const { username, password, email, first_name, last_name, date_of_birth, gender, city, state, country, registration_date, last_login_date, account_status, permissions} = req.body  
 
-    const confirmedUserID = await isUserIDUnique(userID);
 
+    const { userID: confirmedUserID, error: errorActual } = await isUserIDUnique(userID);
     const newUser = await pool.query('INSERT INTO users(user_id, username, password, email, first_name, last_name) VALUES($1, $2, $3, $4, $5, $6)', [confirmedUserID, username, password, email, firstName, lastName]);
-    
-    return newUser
+    return { newUser, errorActual }
 
 }
 
